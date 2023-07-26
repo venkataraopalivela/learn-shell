79833b015044ebb601b5e3b4ec2af3bad15ca104
venkat <venkatapalivela.sap@gmail.com>
2023-07-26 16:32:56 +0530

    Print colour codes

---
+++ print.sh
@@ -0,0 +1,19 @@
+echo  Hello World
+# Red       31
+# Green     32
+# Yellow    33
+# Blue      34
+# Magenta   35
+# Cyan      36
+
+# Syntax how to use the colour code
+# echo -e "\e[CODEmMESSAGE\e[0m"
+
+echo -e "\e[31mHello World\e[0m"
+echo -e "\e[32mHello World\e[0m"
+echo -e "\e[32mHello World\e[0m"
+echo -e "\e[33mHello World\e[0m"
+echo -e "\e[34mHello World\e[0m"
+echo -e "\e[35mHello World\e[0m"
+echo -e "\e[36mHello World\e[0m"
+
